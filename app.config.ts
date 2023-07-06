bfc31109d14c17b49922a434a8dcf7dcba658361
muranuun <mura3-2033@hotmail.co.jp>
2023-07-06 14:50:54 +0900

    customized footer

--- app.config.ts
+++ app.config.ts
@@ -18,12 +18,17 @@ export default defineAppConfig({
     },
     footer: {
       credits: {
-        enabled: false, // possible value are : true | false
-        repository: 'https://www.github.com/nuxt-themes/alpine' // our github repository
+        // possible value are : true | false
+        enabled: false,
+
+        // our github repository
+        repository: '',
+
+        text: ''
       },
-      navigation: true, // possible value are : true | false
+      navigation: false, // possible value are : true | false
       alignment: 'center', // possible value are : 'none' | 'left' | 'center' | 'right'
-      message: 'Follow me on' // string that will be displayed in the footer (leave empty or delete to disable)
+      message: '' // string that will be displayed in the footer (leave empty or delete to disable)
     },
     socials: {
       twitter: 'muranuun_tech',
