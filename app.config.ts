dab398a505e09354a00b05f84d89a6a3361d26fb
muranuun <mura3-2033@hotmail.co.jp>
2023-12-15 21:03:12 +0900

    configured details

--- app.config.ts
+++ app.config.ts
@@ -1,38 +1,42 @@
 export default defineAppConfig({
   alpine: {
-    title: 'muranuun',
-    description: 'The minimalist blog of muranuun',
+    title: 'むらのブログ',
+    description: 'むらの日常をつらつらと',
+
     image: {
       src: '/social-card-preview.png',
       alt: 'An image showcasing my project.',
       width: 400,
       height: 300
     },
+
     header: {
-      position: 'right', // possible value are : | 'left' | 'center' | 'right'
+      position: 'center', // possible value are : | 'left' | 'center' | 'right'
       logo: {
         path: '/logo.svg', // path of the logo
         pathDark: '/logo-dark.svg', // path of the logo in dark mode, leave this empty if you want to use the same logo
-        alt: 'alpine' // alt of the logo
+        alt: 'むらのアイコン' // alt of the logo
       }
     },
+
     footer: {
       credits: {
         // possible value are : true | false
-        enabled: false,
+        enabled: true,
 
         // our github repository
         repository: '',
 
-        text: ''
+        text: 'むらのブログ'
       },
-      navigation: false, // possible value are : true | false
-      alignment: 'center', // possible value are : 'none' | 'left' | 'center' | 'right'
+      navigation: true, // possible value are : true | false
+      alignment: 'left', // possible value are : 'none' | 'left' | 'center' | 'right'
       message: '' // string that will be displayed in the footer (leave empty or delete to disable)
     },
+
     socials: {
-      twitter: 'muranuun_tech',
-      instagram: 'atinuxt',
+      twitter: '',
+      instagram: '',
 
       linkedin: {
         icon: 'uil:linkedin',
@@ -40,10 +44,15 @@ export default defineAppConfig({
         href: 'https://www.linkedin.com/company/nuxtlabs'
       },
 
-      github: 'mura32033'
+      github: ''
     },
+
     form: {
       successMessage: 'Message sent. Thank you!'
+    },
+
+    backToTop: {
+      text: '上に戻る'
     }
   }
 })
