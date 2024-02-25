166f327878e2918e405e52b94aab0600688b38a4
muranuun <mura3-2033@hotmail.co.jp>
2024-02-25 22:30:15 +0900

    changed backgroundColor

--- tokens.config.ts
+++ tokens.config.ts
@@ -1,4 +1,16 @@
 import { defineTheme } from 'pinceau'
 
 export default defineTheme({
+  alpine: {
+    body: {
+      backgroundColor: {
+        initial: '{color.gray.50}',
+        dark: '{color.gray.800}'
+      },
+      color: {
+        initial: '{color.gray.800}',
+        dark: '{color.gray.200}'
+      }
+    }
+  }
 })
