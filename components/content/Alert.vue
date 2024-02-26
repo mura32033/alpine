7d26b112853db792d999db86e842f44c851e1049
muranuun <mura3-2033@hotmail.co.jp>
2024-02-26 23:57:24 +0900

    コンポーネントの調整

---
+++ components/content/Alert.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+defineProps<{
+  type?: string
+}>()
+</script>
+<template>
+  <div :class="`alertCard ${type}`">
+    <div class="alertCard-icon">
+      <Icon v-if="type === 'error'" name="mdi:alert-circle-outline" size="24" />
+      <Icon v-else-if="type === 'warning'" name="mdi:alert" size="24" />
+      <Icon v-else name="mdi:information-outline" size="24" />
+    </div>
+    <div>
+      <slot />
+    </div>
+  </div>
+</template>
+<style>
+  .alertCard {
+    display: flex;
+    flex-direction: column;
+    margin-top: 1rem;
+    margin-bottom: 1rem;
+    padding: 1rem;
+    color: white;
+    border: 1px solid #718096;
+    border-radius: 0.375rem;
+    background-color: #2d374880;
+    text-align: center;
+  }
+  .alertCard-icon {
+    margin: auto;
+  }
+  .warning {
+    background-color: #f59e0bc0;
+    border-color: #d97706;
+  }
+  .error {
+    background-color: #f56565c0;
+    border-color: #c53030;
+  }
+  .info {
+    background-color: #718096c0;
+    border-color: #4a5568;
+  }
+</style>
---
+++ components/content/RecentArticle.vue
@@ -0,0 +1,64 @@
+<script setup lang="ts">
+import { withTrailingSlash } from 'ufo'
+
+const props = defineProps({
+  path: {
+    type: String,
+    default: 'articles'
+  }
+})
+
+// @ts-ignore
+const { data: _articles } = await useAsyncData('articles', async () => await queryContent('/articles/').sort({ date: -1 }).limit(1).find())
+
+const articles = computed(() => _articles.value || [])
+</script>
+
+<template>
+  <div v-if="articles?.length" class="articles-list">
+    <div class="featured">
+      <ArticlesListItem :article="articles[0]" :featured="true" />
+    </div>
+  </div>
+  <div v-else class="tour">
+    <p>表示できる記事がありません。</p>
+  </div>
+</template>
+
+<style scoped lang="ts">
+css({
+  '.articles-list': {
+    '@sm': {
+      px: '{space.12}',
+    },
+    '@md': {
+      px: 0,
+    },
+    '.featured': {
+      my: '{space.12}',
+      '@md': {
+        my: '{space.8}',
+      }
+    },
+    '.layout': {
+      display: 'grid',
+      gridTemplateColumns: 'repeat(1, minmax(0, 1fr))',
+      gap: '{space.12}',
+      '@md': {
+        gridTemplateColumns: 'repeat(2, minmax(0, 1fr))',
+        gap: '{space.8}',
+      },
+      '@lg': {
+        gridTemplateColumns: 'repeat(3, minmax(0, 1fr))',
+      },
+    }
+  },
+  '.tour': {
+    minHeight: '30vh',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+})
+</style>
