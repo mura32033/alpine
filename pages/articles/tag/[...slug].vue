4f04b6053648d7799bfc13562bb9f1cf70adf00c
muranuun <mura3-2033@hotmail.co.jp>
2024-05-17 21:11:48 +0900

    added tag slug

---
+++ pages/articles/tag/[...slug].vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'docs'
+})
+const route = useRoute()
+const { data: page } = await useAsyncData('tag_articles', () => queryContent('articles').where({
+  tags: route.params.slug
+}).find())
+</script>
+
+<template>
+  <div>
+    {{ page }}
+  </div>
+</template>
